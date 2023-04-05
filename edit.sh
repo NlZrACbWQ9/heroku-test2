3c157710e4403063c02e575a24d3763dc76711e3
Heather Brown <129369959+NlZrACbWQ9@users.noreply.github.com>
2023-04-05 17:18:31 +0800

    Update edit.sh

--- edit.sh
+++ edit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-envsubst < /root/config.json.tp > /root/config.json
-envsubst '\$PORT' < /root/nginx.template.conf > /root/nginx.conf
+sed "s/\$UUID/${UUID}/g" /root/config.json
+sed "s/\$PORT/${PORT}/g" /root/nginx.conf
